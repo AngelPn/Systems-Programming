4bd7c25224089511bbb7f5d65878ab19723bc9c4
AngelPn <aggi.pa@gmail.com>
2021-04-22 15:41:06 +0300

    create_infiles bash

--- travelMonitor/create_infiles.sh
+++ travelMonitor/create_infiles.sh
@@ -12,9 +12,6 @@ inputFile=$1
 input_dir=$2
 numFilesPerDirectory=$3
 
-# # Go to testFiles directory
-# cd testFiles
-
 # Check if inputFile exists
 # and if the numFilesPerDirectory is greater than 0
 if [ ! -e ${inputFile} ]; then
@@ -33,103 +30,50 @@ else
     exit 1
 fi
 
+# Go through each line of inputFile
+while IFS= read -r line; do
+    # Split line string to get the country
+    IFS=' ' read -ra words <<< "$line"
+    country=${words[3]}
 
-# Create arrays with the viruses and countries
-viruses+=($(cat ${virusesFile}))
-countries+=($(cat ${countriesFile}))
-
-# Create array of IDs: ID is a number from 1 to 9999
-# If duplicates are not allowed or numLines < 10000, generate unique IDs
-if [ ${duplicatesAllowed} -eq "0" -a ${numLines} -le "10000" ]; then
-    ids=($(shuf -i 1-9999 -n $numLines))
-# Else generate IDs randomly with duplicates
-else
-    if [ ${duplicatesAllowed} -eq "0" ]; then
-        echo "Given numLines > 10K, so duplicates will be allowed..."
+    # Go to input dir and create the subdir for country if it is not already there
+    cd ${input_dir}
+    if [ ! -e ${country} ]; then
+        mkdir ${country}
     fi
-    # Get shuffled IDs
-    shuf_ids=($(shuf -i 1-9999))
 
-    for ((i = 0; i < numLines; )); do
-        # x represents how many times the ID will be duplicated
-        if [ ${numLines} -gt "10000" -a ${#shuf_ids[@]} -eq "1" ]; then
-            x=$((numLines-i))
-        else
-            x=$((RANDOM % 20 + 1))
-        fi
-        
-        for ((j = 0; j < x; j++)); do
-            ids[i]=${shuf_ids[0]}
-            i=$(( $i + 1 ))
-        done
-        # Remove first element in shuf_ids
-        shuf_ids=("${shuf_ids[@]:1}")
-    done
-fi
+    # Go to country dir and store all records of this country to country-file
+    cd ${country}
+    echo $line >> "$country-file"
 
-# Return random string of letters of length given in argument
-function rand-str() {
-    # -dc: delete all except given set
-    tr -dc a-z </dev/urandom | head -c $1
-}
+    cd ../..
+done < "$inputFile"
 
-# Return randomly YES or NO
-function yes-no() {
-    if [ $1 -eq "0" ]; then
-        echo "YES"
-    else
-        echo "NO"
-    fi
-}
+# Go to input dir
+cd ${input_dir}
+# Loop through country directories in input dir
+for country in * ; do
+    # Go to country dir
+    cd ${country}
+    # Get the number of records in country file
+    num_recs=${#country[@]}
 
-# Create inputFile
-touch inputFile
+    # For each of the records in country file,
+    # distribute round-robin the lines in files 
+    i=1
+    while IFS= read -r line; do
+        echo $line >> "$country-$i.txt"
 
-function write-in-File(){
-    index_virus=$((RANDOM % ${#viruses[@]}))
-    virus=${viruses[index_virus]}
-
-    vaccinated=$(yes-no $((RANDOM % 2)))
-
-    if [ "$vaccinated" = "YES" -o $((RANDOM % 2)) -eq "0" ]; then
-        # Create random date
-        dd=$((RANDOM % 31 + 1))
-        mm=$((RANDOM % 12 + 1))
-        yyyy=$((RANDOM % 3 + 2018))
-        # Make sure both day and month are 2 digits
-        if [ $dd -lt "10" ]; then
-           dd="0${dd}"
-        fi
-        if [ $mm -lt "10" ]; then
-           mm="0${mm}"
-        fi
-        date=$dd-$mm-$yyyy
-        echo $1 $virus $vaccinated $date >> inputFile
-    else
-        echo $1 $virus $vaccinated >> inputFile
-    fi
-}
-
-# For the number of lines
-for ((i = 0; i < numLines - 1; i++)); do
-    # Create citizen's record
-    id=${ids[i]}
-    firstname=$(rand-str $((RANDOM % 12 + 3)))
-    lastname=$(rand-str $((RANDOM % 12 + 3)))
-
-    index_country=$((RANDOM % ${#countries[@]}))
-    country=${countries[index_country]}
-
-    age=$((RANDOM % 120 + 1))
-
-    write-in-File "$id $firstname $lastname $country $age"
-
-    # For every time this citizen is duplicated, write in file
-    while [ $(( $i + 1 )) -le ${numLines} -a "$id" = "${ids[i+1]}" ]; do
-        write-in-File "$id $firstname $lastname $country $age"
         i=$(( $i + 1 ))
-    done
+        if [ ${i} -gt ${numFilesPerDirectory} ]; then
+            i=1
+        fi
+    done < "$country-file"
 
+    # Remove country-file
+    rm -f "$country-file"
+    
+    cd ..
 done
 
-cd ../
+cd ..
