436817e889e4079cc30a4f8726255afded120619
AngelPn <aggi.pa@gmail.com>
2021-04-24 17:49:36 +0300

    monitor in progress

---
+++ travelMonitor/monitor/monitor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <dirent.h> 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/select.h>
+
+int main(int argc, char **argv){
+
+}
