673b4141f26b02c07f08fc02bd66ba01716c630d
AngelPn <aggi.pa@gmail.com>
2021-05-12 00:32:18 +0300

    init

--- travelMonitor/monitor/relations/country.h
+++ travelMonitor/monitor/relations/country.h
@@ -10,16 +10,6 @@ country create_country(char *country_name);
 /* Returns a pointer to country's name */
 void *get_country_name(void *c);
 
-/* Change the values of population query in country */
-void increase_vaccinated_persons(country c);
-void increase_not_vaccinated_persons(country c);
-void increase_popByAge_vaccinated(country c, int age);
-void increase_popByAge_not_vaccinated(country c, int age);
-
-/* Prints data for population queries */
-void populationStatus(void *item, int key);
-void popStatusByAge(void *item, int key);
-
 /* Print the name of the country */
 void print_country(void *c);
 
